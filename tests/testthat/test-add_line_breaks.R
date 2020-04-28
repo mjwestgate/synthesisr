3f399c8e61d0539800c0ceb3d13e65f103192f8e
Eliza Grames <eliza.grames@uconn.edu>
2020-04-28 12:17:32 -0400

    set up empty files for all unit tests

--- tests/testthat/test-add_line_breaks.R
+++ tests/testthat/test-add_line_breaks.R
@@ -1,9 +1,9 @@
 title <- "On the Origin of Species by Means of Natural Selection, or the Preservation of Favoured Races in the Struggle for Life"
-
-lines_added <- add_line_breaks(title, n = 20)
+N <- 20
+lines_added <- add_line_breaks(title, n = N)
 
 split_text <- strsplit(lines_added, "\n")[[1]]
 
 nchars <- unlist(lapply(gsub(" ", "", split_text), nchar))
 
-expect((all(nchars)<20), failure_message = "Expected number of characters not found in groups split by add_line_breaks")
+expect((all(nchars)<N), failure_message = "Expected number of characters not found in groups split by add_line_breaks")
