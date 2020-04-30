e08bbfdd24092dce215060a4c78f45f1e5cfbe03
Eliza Grames <eliza.grames@uconn.edu>
2020-04-30 09:35:56 -0400

    updated format_citation to coerce objects to data.frames first so that output is identical

--- R/format_citation.R
+++ R/format_citation.R
@@ -17,123 +17,15 @@ format_citation <- function(
   line_breaks = FALSE,
   ...
 ){
-  UseMethod("format_citation")
+  if(!(class(data) %in% c("data.frame", "bibliography", "list"))){
+  stop(print("format_citation expects input data to be an object of class data.frame, bibliography, or list"))
+    }
+
+  if(class(data)!="data.frame"){
+    data <- as.data.frame(data)
   }
 
-
-#' @describeIn format_citation Format citation from a list
-format_citation.list <- function(
-	data, # list of data from a standard import function
-  details = TRUE, # whether to allow or suppress bibliographic details - name, year, journal
-	abstract = FALSE, # option to return only the citation for an article
-  add_html = FALSE,
-  line_breaks = FALSE,
-  ...
-){
-	if(!details){
-    result <- as.character(data["title"])
-  }else{
-		if(any(names(data) == "author")){
-			# author info
-			# remove any additional characters that display affiliations (i.e. those after last ".")
-			author.data <- unlist(lapply(
-        strsplit(as.character(data$author), ""),
-        function(a){
-  				dot.lookup <- a %in% "."
-  				if(any(dot.lookup)){
-            a <- a[seq_len(max(which(dot.lookup)))]
-          }
-  				return(paste(a, collapse = ""))
-  			}
-      ))
-			if(any(grepl(",", author.data))){
-				author.data <- unlist(lapply(
-          strsplit(author.data, ", "),
-          function(a){paste(a[2], a[1], sep = " ")}
-        ))
-			}
-			n.authors <- length(data$author)
-			if(n.authors >= 4){
-        n.authors <- 4
-      }
-			author.info <- switch(as.character(n.authors),
-				"0" = "Anon.",
-				"1" = author.data,
-				"2" = {paste(author.data, collapse = " & ")},
-				"3" = {paste0(author.data[1], ", ", author.data[2], " & ", author.data[3])},
-				"4" = {paste0(author.data[1], ", ", author.data[2], " et al.")})
-		}
-  # }
-		# paste info in the correct order
-		lookup.headers <- c("year", "title", "journal", "volume", "pages")
-		lookup.result <- lookup.headers %in% names(data)
-		if(all(lookup.result)){
-			result <- paste0(
-        author.info,
-        " (", data$year, ") ",
-				data$title, ". ",
-        tools::toTitleCase(tolower(data$journal)),
-        " ",
-        data$volume,
-        ": ",
-        data$pages
-      )
-		}else{
-			result <- paste(
-        author.info,
-        paste(data[lookup.headers[lookup.result]], collapse = " "),
-        sep = " "
-      )
-    }
-	  # note - the above doesn't add brackets around year
-  }
-	# add abstract if required
-	if(abstract & any(names(data) == "abstract")){
-		result <- paste0(result, ".<br><br><strong>Abstract</strong><br>", data$abstract)
-	}
-
-  if(is.logical(line_breaks)){
-    if(line_breaks){
-      result <- add_line_breaks(result)
-    }
-  }else{
-    if(is.numeric(line_breaks)){
-      result <- add_line_breaks(result, line_breaks)
-    }
-  }
-
-return(result)
-}
-
-#' @describeIn format_citation Format citation from a bibliography
-format_citation.bibliography <-  function(
-	data,
-  details = TRUE,
-	abstract = FALSE,
-  add_html = FALSE,
-  line_breaks = FALSE,
-  ...
-){
-  lapply(data, function(a, details, abstract, add_html, line_breaks){
-    format_citation.list(a, details, abstract, add_html, line_breaks)
-    },
-    details = details,
-    abstract = abstract,
-    add_html = add_html,
-    line_breaks = line_breaks
-  )
-}
-
-#' @describeIn format_citation Format citation from a data.frame
-format_citation.data.frame <- function(
-  data,
-  details = TRUE,
-  abstract = FALSE,
-  add_html = FALSE,
-  line_breaks = FALSE,
-  ...
-){
-  colnames(data) <- clean_names(colnames(data))
+    colnames(data) <- clean_names(colnames(data))
   if(any(names(data) == "journal")){
     source <- "journal"
   }else{
@@ -219,4 +111,4 @@ format_citation.data.frame <- function(
   }
   data_out <- unlist(lapply(data_out, trimws))
   return(data_out)
-}
+  }
